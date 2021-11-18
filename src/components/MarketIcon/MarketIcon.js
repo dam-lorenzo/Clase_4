fbbfa1db3db18ddcb6834cfcf7e5b6af427fbb25
dam-lorenzo <85456088+dam-lorenzo@users.noreply.github.com>
2021-11-18 20:17:56 -0300

    .

---
+++ src/components/MarketIcon/MarketIcon.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Icon } from 'semantic-ui-react'
+import './MarketIcon.css'
+
+function MarketIcon() {
+    return (
+        <Icon bordered inverted color='black' name='shopping cart' />
+    )
+}
+
+export default MarketIcon
